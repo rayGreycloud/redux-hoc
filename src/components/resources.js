f6a0effda1d9d26404c9a562cc5a766592d652c0
rayGreycloud <rayGreycloud@gmail.com>
2016-07-21 13:32:58 -0700

    Add basic components for demo app

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Recipe
+      <ul>
+        <li>1 Cup Sugar</li>
+        <li>1 Cup Flour</li>
+        <li>1 Cup Milk</li>
+      </ul>
+    </div>
+  );
+}
